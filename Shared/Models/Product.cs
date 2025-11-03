f7b3c1b931422b8138e42e108701f86c77c43977
ThabangManaka <thabangdansteyn@gmail.com>
2025-11-03 22:19:16 +0200

    Added Orderdb Context and migrations

--- Shared/Models/Product.cs
+++ Shared/Models/Product.cs
@@ -10,6 +10,7 @@ namespace Shared.Models
 
         public string? Name { get; set; }
         [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }  
+        public decimal Price { get; set; }
     }
+
 }
