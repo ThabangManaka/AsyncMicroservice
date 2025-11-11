2f617c1bfeea9eb052bc2fd7aacfd33c140d5ed2
ThabangManaka <thabangdansteyn@gmail.com>
2025-11-11 16:56:16 +0200

    Added  Dto's Service Response and Product  interface

---
+++ ProductApi/Repository/IProduct.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DTOs;
+using Shared.Models;
+
+namespace ProductApi.Repository
+{
+    public interface IProduct
+    {
+        Task<ServiceResponse> AddProductAsync(Product product);
+        Task<List<Product>> GetAllProductsAsync();
+    }
+}
---
+++ ProductApi/Repository/ProductRepo.cs
@@ -0,0 +1,20 @@
+﻿using ProductApi.Data;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace ProductApi.Repository
+{
+    public class ProductRepo(ProductDbContext context) : IProduct
+    {
+
+        public async Task<ServiceResponse> AddProductAsync(Product product)
+        {
+
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+
+        }
+    }
+}
